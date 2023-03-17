264bceb74c76dd94a64cfd794fb828de6f7203ac
Erick Koga <adm@erickkoga.com>
2023-03-17 20:39:35 -0300

    Added environment variables.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import fastify from 'fastify';
+import config from './config/config';
 
+// Instantiate the Fastify Framework.
 const server = fastify();
 
-server.listen({ port: 8080 }, (err, address) => {
+// Start the server on the assigned port.
+server.listen({ port: config.API_PORT }, (err, address) => {
+  // If any errors occur, throw it on the console and exit the process.
   if (err) {
     console.error(err);
     process.exit(1);
   }
+  // If the server starts successfully, report the address and port on the console.
   console.log(`Server listening at ${address}`);
 });
